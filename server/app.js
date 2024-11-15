cca70382c6d0f14eff0692015c51307406cebe7e
shreyaschaudhari04 <shreyasrchaudhari@gmail.com>
2024-11-15 20:41:23 +0530

    Made changes for backend

--- server/app.js
+++ server/app.js
@@ -5,29 +5,26 @@ require('dotenv').config();
 const app = express();
 const router = require('./routes/router');
 
-
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' 
-    ? 'https://erino-crud-frontend.vercel.app/' 
-    : 'http://localhost:5173', 
+  origin: '*',
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   allowedHeaders: "Content-Type, Authorization",
-  preflightContinue: false,  
+  preflightContinue: false,
 };
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200); 
+  res.sendStatus(200);
 });
 
 app.use(express.json());
-app.use(router);  
+app.use(router);
 
-require('./db/connection');  
-const users = require('./models/userSchema');  
+require('./db/connection');
+const users = require('./models/userSchema');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
