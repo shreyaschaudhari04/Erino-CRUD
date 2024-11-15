a19a4809e5adb837309e39959f141983febe1380
shreyaschaudhari04 <shreyasrchaudhari@gmail.com>
2024-11-15 19:02:14 +0530

    Made Changes to app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(router);
 require("./db/connection");
 const users = require("./models/userSchema");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB = "mongodb+srv://shreyasrchaudhari:mBqyG0sJwyt2Q2n1@cluster0.uxajh.mongodb.net/Erino?retryWrites=true&w=majority&appName=Cluster0"
+const DB = process.env.DB
 
 mongoose.connect(DB, {
     useNewUrlParser:true,
